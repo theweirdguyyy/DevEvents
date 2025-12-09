88f03f364ff121282857e0f9738e797d8121a247
theweirdguyyy <theweirdguyyyy@gmail.com>
2025-12-09 22:19:26 +0600

    deploy try 2

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,12 @@ import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
 
-// Remove the BASE_URL definition since we are using a relative path
-// const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
     'use cache';
     cacheLife('hours')
-    
-    // 💡 Fix: Change the fetch URL to a relative path
-    const response = await fetch(`/api/events`); 
-    
-    // Note: If you were fetching an external API, you would 
-    // use a server-only environment variable (without NEXT_PUBLIC)
-    // and would not face this issue if correctly set in Vercel.
-    
+    const response = await fetch(`${BASE_URL}/api/events`);
     const { events } = await response.json();
 
     return (
--- components/LightRays.tsx
+++ components/LightRays.tsx
@@ -445,7 +445,7 @@ void main() {
   return (
     <div
       ref={containerRef}
-      className={`pointer-events-none relative z-[3] h-full w-full overflow-hidden ${className}`.trim()}
+      className={`pointer-events-none relative z-3 h-full w-full overflow-hidden ${className}`.trim()}
     />
   );
 };
